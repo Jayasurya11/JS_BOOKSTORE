cdc24ac6e97ef1adaee6d7ef5b32ba12d592258e
Jayasurya11 <suryakohli11@gmail.com>
2024-01-15 14:14:09 +0530

    final frontend

--- client/src/home/PromoBanner.jsx
+++ client/src/home/PromoBanner.jsx
@@ -1,6 +1,6 @@
 
 import bookPic from "../assets/awardbooks.png";
-import { Link } from "react-router-dom";
+
 const PromoBanner = () => {
   return (
     <div className="mt-16 py-12 bg-teal-100 px-4 lg:px-24 ">
@@ -10,8 +10,7 @@ const PromoBanner = () => {
             2023 National Book Awards for Self-Help Shortlist
           </h2>
 
-          <Link to="/success">Success</Link>
-          <Link to="/cancel">Cancel</Link>
+          
           
         </div>
         <div>
